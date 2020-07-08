d869bb25c0e851249b728e16bb285ab438a12272
Aurelie Verrot <aurelie.verrot@gmail.com>
2020-07-08 10:01:41 -0700

    fixed paths in header and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  
+  # get 'users/new'
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # You can have the root of your site routed with "root"
+  resources :users
   
 
 
